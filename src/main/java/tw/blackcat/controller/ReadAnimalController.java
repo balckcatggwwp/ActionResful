3ab8bf16e93fe4746dc372e16bf2a433f5e54eb5
Black <z455581@gmail.com>
2025-04-01 11:47:39 +0800

    no message

---
+++ src/main/java/tw/blackcat/controller/ReadAnimalController.java
@@ -0,0 +1,42 @@
+package tw.blackcat.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import tw.blackcat.model.Animal;
+
+@RestController
+public class ReadAnimalController {
+	ArrayList<Animal>  animals = new ArrayList<Animal>();
+	public  ReadAnimalController() {
+		animals.add(new Animal(1,"monkey"));		
+		animals.add(new Animal(2,"dog"));
+		animals.add(new Animal(3,"cat"));
+		animals.add(new Animal(4,"bird"));
+		animals.add(new Animal(5,"pig"));//10個
+		animals.add(new Animal(6,"tiger"));
+		animals.add(new Animal(7,"lion"));
+		animals.add(new Animal(8,"elephant"));
+		animals.add(new Animal(9,"mouse"));
+		animals.add(new Animal(10,"snake"));
+	}
+	@PostMapping("/animalreader.controller")
+	public List<Animal> processReadAnimalAction(@RequestParam("keyword") String animalName){
+		return searchResult(animalName);
+	}
+
+	private List<Animal> searchResult(String animalName) {
+        ArrayList<Animal> result = new ArrayList<Animal>();
+        
+        for(Animal a: animals) {
+        	if(animalName.length()!=0 && a.getAname().contains(animalName)) {
+        		result.add(a);
+        	}
+        }
+		return result;
+	}
+}
